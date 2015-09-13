a109b4a97824dbcb3f48b5714002e5c9d8e67409
Lyn Han <lynfhan@gmail.com>
2015-09-13 13:39:22 -0500

    num anim

--- base/static/base/main.js
+++ base/static/base/main.js
@@ -12,55 +12,50 @@ $(document).ready(function() {
     $('.messages').hide();
   }, 3000);
 
-  // var allnum = $('.green');
-  // var values = [];
-  //
-  // for (var i = 0; i < allnum.length; i++) {
-  //   values.push(parseInt($(allnum[i]).text()));
-  // }
-  //
-  // for (var i = 0; i <= values[0]; i++) {
-  //   console.log(values[0]);
-  //   (function(e) {
-  //     setTimeout(function() {
-  //       $(allnum[0]).text(e);
-  //   }, 90 * e);
-  //   })(i);
-  // }
-  //
-  // for (var i = 0; i <= values[1]; i++) {
-  //   console.log(values[1]);
-  //   (function(e) {
-  //     setTimeout(function() {
-  //       $(allnum[1]).text(e);
-  //   }, 90 * e);
-  //   })(i);
-  // }
-  //
-  // for (var i = 0; i <= values[2]; i++) {
-  //   console.log(values[2]);
-  //   (function(e) {
-  //     setTimeout(function() {
-  //       $(allnum[2]).text(e);
-  //   }, 90 * e);
-  //   })(i);
-  // }
-  //
-  // for (var i = 0; i <= values[3]; i++) {
-  //   console.log(values[3]);
-  //   (function(e) {
-  //     setTimeout(function() {
-  //       $(allnum[3]).text(e);
-  //   }, 90 * e);
-  //   })(i);
-  // }
-  //
-  // for (var i = 0; i <= values[4]; i++) {
-  //   console.log(values[4]);
-  //   (function(e) {
-  //     setTimeout(function() {
-  //       $(allnum[4]).text(e);
-  //   }, 90 * e);
-  //   })(i);
-  // }
+  var allnum = $('.green');
+  var values = [];
+
+  for (var i = 0; i < allnum.length; i++) {
+    values.push(parseInt($(allnum[i]).text()));
+  }
+
+  for (var i = 0; i <= values[0]; i++) {
+    (function(e) {
+      setTimeout(function() {
+        $(allnum[0]).text(e);
+    }, (1000 / values[0]) * e);
+    })(i);
+  }
+
+  for (var i = 0; i <= values[1]; i++) {
+    (function(e) {
+      setTimeout(function() {
+        $(allnum[1]).text(e);
+    }, (1000 / values[1]) * e);
+    })(i);
+  }
+
+  for (var i = 0; i <= values[2]; i++) {
+    (function(e) {
+      setTimeout(function() {
+        $(allnum[2]).text(e);
+    }, (1000 / values[2]) * e);
+    })(i);
+  }
+
+  for (var i = 0; i <= values[3]; i++) {
+    (function(e) {
+      setTimeout(function() {
+        $(allnum[3]).text(e);
+    }, (1000 / values[3]) * e);
+    })(i);
+  }
+
+  for (var i = 0; i <= values[4]; i++) {
+    (function(e) {
+      setTimeout(function() {
+        $(allnum[4]).text(e);
+    }, (1000 / values[4]) * e);
+    })(i);
+  }
 });
